ffa7d9751e4eb955e9ddee9ed7a6bd5baeac29d3
nasirkhan <nasirkhana842@gmail.com>
2024-02-22 05:24:18 -0500

    show the message

--- src/UseReducer.jsx
+++ src/UseReducer.jsx
@@ -1,56 +1,66 @@
 import React, {
-  useReducer,
-  useState
+useReducer,
+useState
 } from "react";
 import { reducer } from "./reducer";
 const UseReducer = () => {
-  const [todo, setTodo] = useState()
-  const initialState = {
-    todos: [],
-    error: false,
-    success: false,
-    message: '',
-  }
-  const handleClick = (e) => {
-    e.preventDefault();
-    if (!todo) {
-      dispatch({
-        type: 'EMPTY'
-      })
-    } else{
-      dispatch({type:"ADD_TODO", payload:todo})
-    }
-    setTimeout(() => {
-      dispatch({
-        type: 'REMOVE_MESSAGE'
-      })
-    }, 2000)
+const [todo, setTodo] = useState()
+const initialState = {
+todos: [],
+error: false,
+success: false,
+message: '',
+}
+const handleClick = (e) => {
+e.preventDefault();
+if (!todo) {
+dispatch({
+type: 'EMPTY'
+})
+} else{
+dispatch({type:"ADD_TODO", payload:todo})
+}
+setTimeout(() => {
+dispatch({
+type: 'REMOVE_MESSAGE'
+})
+}, 2000)
+setTodo('')
 
-  }
-
-  const [state, dispatch] = useReducer(reducer, initialState);
-  return ( 
-  <>
-    <
-    div className = "container col-lg-5 mx-auto shadow p-3" >
-    <form >
-    <input type = "text"
-    className = "form-control"
-    placeholder = "enter your todo..."
-    value = {todo}
-    onChange = {
-      (e) => setTodo(e.target.value)
-    }
-    /> 
-    {state.error && <p className="text-danger fw-bold">
-      {state.message}</p>}
-    {state.success && <p className="text-success fw-bold">
-      {state.message}</p>}
-    <button onClick = {
-      handleClick} className = "btn btn-success w-100 my-2" > Add </button> </form> 
-      </div>
-     </>
-  );
 }
 
-export default UseReducer;
+const [state, dispatch] = useReducer(reducer, initialState);
+return (
+<>
+  < div className="container col-lg-5 mx-auto shadow p-3">
+    <form>
+      <input type="text" className="form-control" placeholder="enter your todo..." value={todo} onChange={ (e)=>
+      setTodo(e.target.value)
+      }
+      />
+      {state.error && <p className="text-danger fw-bold">
+        {state.message}</p>}
+      {state.success && <p className="text-success fw-bold">
+        {state.message}</p>}
+      <button onClick={ handleClick} className="btn btn-success w-100 my-2"> Add
+      </button>
+    </form>
+    </div>
+
+    <div className="container mx-auto">
+      <div className="row">
+        {state.todos.map((item)=>{
+        return <>
+          <div className="card">
+            {item}
+          </div>
+        </>
+        })}
+      </div>
+    </div>
+  </>
+
+  );
+  }
+
+  export default UseReducer;
